75ede50978e3bfc59ca99d49de1b770ebec3ea77
Chirag <chirunder@gmail.com>
2024-06-07 14:02:23 +0530

    fixed url

--- Smart Query Interface/static/script.js
+++ Smart Query Interface/static/script.js
@@ -1,4 +1,4 @@
-const url = "http://127.0.0.1:5000/api/sessions"
+const url = "/api/sessions"
 
 //Environments and Databases
 const environments = ["dev", "Hyperface User Acceptance Testing Environment", "Prod"];
